5701c82be68db60bff2eecb03c88ca19ac35c53e
nadezhda <n.dobritckaia@semrush.com>
2017-09-01 00:08:41 +0300

    task 1

---
+++ src/Lesson_1.java
@@ -0,0 +1,54 @@
+/**
+ * Created by nadezhda on 29/08/17.
+ */
+public class Lesson_1 {
+
+    public static void main(String[] args) {
+
+//        int a = 5;
+//        int b = 10;
+//        int c;
+//        c = a + b;
+//
+//        System.out.println(c);
+//        System.out.println("Сумма = " + (a + b));
+//        System.out.println(c++);
+//        System.out.println(++c);
+//        System.out.println("\"экранирование символов\"");
+
+        int a = 5;
+        int b = 10;
+        int x = (0 - b) / a;
+
+        System.out.println(x);
+
+
+
+//        Scanner sc = new Scanner(System.in);
+//        int a;
+//        int b;
+//        System.out.println("How much is a?");
+//        a = sc.nextInt();
+//        System.out.println("How much is b?");
+//        b = sc.nextInt();
+//
+//        System.out.println("x = " + (0 - b) / a);
+
+
+//        Scanner sc = new Scanner(System.in);
+//        int a;
+//        int b;
+//        int c;
+//        System.out.println("How much is a?");
+//        a = sc.nextInt();
+//        System.out.println("How much is b?");
+//        b = sc.nextInt();
+//        System.out.println("How much is c?");
+//        c = sc.nextInt();
+
+
+            //       System.out.println("x = " + );
+
+      //  }
+    }
+}
