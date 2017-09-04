34255905b9ab367bc26b724aed9fb5a702d004f6
nadezhda <n.dobritckaia@semrush.com>
2017-09-04 23:14:21 +0300

    task 2

--- src/Lesson_1.java
+++ src/Lesson_1.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Lesson_1 {
 
     public static void main(String[] args) {
+
 //disregard//
 
 //        int a = 5;
@@ -19,59 +20,53 @@ public class Lesson_1 {
 //        System.out.println(++c);
 //        System.out.println("\"экранирование символов\"");
 
-/** 1. Написать программу осуществляющую решение линейного уравнения ax + b = 0.
- a и b задаете в программе, х выводите на экран. */
-
-        int a = 5;
-        int b = 10;
-        int x = (0 - b) / a;
-
-        System.out.println(x);
-
-/** 2. В первую задачу добавьте возможность пользователю самому вводить данные a и b.
- Используйте класс Scanner.*/
-
-        Scanner sc = new Scanner(System.in);
-        int c;
-        int d;
-        System.out.println("How much is c?");
-        c = sc.nextInt();
-        System.out.println("How much is d?");
-        d = sc.nextInt();
-
-        System.out.println("x = " + (0 - d) / c);
-
-///** 3. Напишите программу вычисляющую квадратное уравнение ax*x + b * x + c = 0.
-// Коэффициенты вводятся с клавиатуры.*/
+///** 1. Написать программу осуществляющую решение линейного уравнения ax + b = 0.
+// a и b задаете в программе, х выводите на экран. */
+//
+//        int a = 5;
+//        int b = 10;
+//        int x = (0 - b) / a;
+//
+//        System.out.println(x);
+//
+///** 2. В первую задачу добавьте возможность пользователю самому вводить данные a и b.
+// Используйте класс Scanner.*/
 //
 //        Scanner sc = new Scanner(System.in);
-//        int a, b, c;
-//
-//        System.out.println("How much is a?");
-//        a = sc.nextInt();
-//        System.out.println("How much is b?");
-//        b = sc.nextInt();
+//        int c;
+//        int d;
 //        System.out.println("How much is c?");
 //        c = sc.nextInt();
+//        System.out.println("How much is d?");
+//        d = sc.nextInt();
 //
-//        private static int discriminant(){
-//            int discriminant;
-//            if(discriminant > 0){
-//
-//            }
-//            else if(discriminant == 0){
-//
-//            }
-//            else if(discriminant < 0){
-//                System.out.println("No solution.");
-//            }
-//
-//
-//        }
+//        System.out.println("x = " + (0 - d) / c);
+
+/** 3. Напишите программу вычисляющую квадратное уравнение ax*x + b * x + c = 0.
+ Коэффициенты вводятся с клавиатуры.*/
 
 
-            //       System.out.println("x = " + );
+        Scanner sc = new Scanner(System.in);
+        int a, b, c;
+        System.out.println("How much is a?");
+        a = sc.nextInt();
+        System.out.println("How much is b?");
+        b = sc.nextInt();
+        System.out.println("How much is c?");
+        c = sc.nextInt();
+        int discriminant = (b * b) - (4 * a * c);
 
-      //  }
+        if (discriminant > 0) {
+            double x1, x2;
+            x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            System.out.println("x1 = " + x1 + ", x2 = " + x2);
+        } else if (discriminant == 0) {
+            int x = -b / (2 * a);
+            System.out.println("x = " + x);
+        } else if (discriminant < 0) {
+            System.out.println("No solution.");
+        }
     }
 }
+
---
+++ src/Lesson_2.java
@@ -0,0 +1,142 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by nadezhda on 01/09/17.
+ */
+public class Lesson_2 {
+
+    public static void main(String[] args) {
+
+
+//        byte s = 1;
+//        s = (byte) (s + 3);
+//        s += 3;
+//
+//        boolean s1 = false;
+//        boolean s2 = true;
+//        System.out.println(s1 & s2);
+//
+//        String s3 = "Hi";
+//        if (s3 != "Hi") {
+//            System.out.println("Equals");
+//        } else {
+//            if (args.length == 0) {
+//                System.out.println("Not equals");
+//            } else {
+//                System.out.println("Not equals 2");
+//            }
+//        }
+//        System.out.println("Not equals 2");
+
+
+//        int a = 45, b = 44, c = 46;
+//
+//        if (a > b) {
+//            if (a > c){
+//                System.out.println("A");
+//            } else {
+//                System.out.println("C");
+//            }
+//        } else if (b > c){
+//            System.out.println("B");
+//        } else {
+//            System.out.println("C");
+//        }
+
+//
+//        for (int i = 0; i < 20; i++){
+//            System.out.println("I = " + i);
+//        }
+//
+//        int o = 0;
+//        while (o < 20){
+//            System.out.println("Hi");
+//            o += 5;
+//        }
+//
+//        Scanner scanner = new Scanner(System.in);
+//            while (!scanner.hasNextDouble()){
+//                System.out.println("Error");
+//                scanner.next();
+//            }
+//        System.out.println("Number " + scanner.nextDouble());
+//        }
+
+//        int[] a = new int[10];
+//        a[2] = 3;
+//        System.out.println(a[2]);
+//
+//        int[] b = new int[] {1, 2, 3, 4};
+//        for (int b = 0; b < a.length; b++) {
+//            System.out.println(a[b] + " ");
+//        }
+    }
+
+/** 1. Напишите программу, округляющую числа.*/
+        public static void round() {
+            //option 1
+            double a = 16.24;
+            double x = a - (int) a;
+            System.out.println("Число " + a + " округляется до ");
+            if (x >= 0.5) {
+                a += 1;
+            } else a = (int) a;
+
+            System.out.println((int) a);
+
+            //option 2
+            double b = 16.24;
+            System.out.println("b = " + Math.round(b));
+    }
+
+/** 2. Напишите программу, которая несколько раз выводит текст разными циклами.*/
+        public static void cycleFor() {
+
+            for (int i = 0; i <5; i++) {
+                System.out.println("Hello world!" + i);
+            }
+        }
+
+        public static void cycleWhile(){
+            int i = 0;
+            while (i < 6){
+                System.out.println("Hey, Jude!" + i);
+            }
+        }
+
+/** 3. Напишите программу, находящую максимум и минимум в массиве. Массив заполняется случайными значениями (класс Random).*/
+        public static void array(){
+
+            int array[] = new int[25];
+
+            for(int i = 0; i < array.length; i++){
+                Random random = new Random();
+                array[i] = random.nextInt(125) + 1;
+            }
+
+            int min = array[0];
+            int max = array[0];
+
+            for(int i = 0; i != array.length; i++){
+                if(array[i] > max){
+                    max = array[i];
+                }
+                if(array[i] < min){
+                    min = array[i];
+                }
+            }
+            System.out.println(max + " " + min);
+        }
+
+
+/** 4. Напишите программу, которая находит простые числа от 0 до 10000. Простые числа делятся на себя и на 1. Полный перебор и решето Эрастофена.*/
+        public static void erastofen(){
+            int array[];
+
+        }
+
+
+
+
+}
